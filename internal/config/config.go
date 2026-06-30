17e6802b80078efa1019a4a2650f0e1ed45f7abc
linfree <linfree.php@outlook.com>
2026-06-30 10:38:59 +0800

    feat: add config management (~/.open-todo/config.json)

---
+++ internal/config/config.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	Port         int    `json:"port"`
+	ServerURL    string `json:"server_url"`
+	AuthToken    string `json:"auth_token"`
+	AutoSync     bool   `json:"auto_sync"`
+	DatabasePath string `json:"database_path"`
+}
+
+func Default() *Config {
+	home, _ := os.UserHomeDir()
+	return &Config{
+		Port:         18080,
+		AutoSync:     false,
+		DatabasePath: filepath.Join(home, ".open-todo", "open-todo.db"),
+	}
+}
+
+func configDir() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	dir := filepath.Join(home, ".open-todo")
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return "", err
+	}
+	return dir, nil
+}
+
+func configPath() (string, error) {
+	dir, err := configDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(dir, "config.json"), nil
+}
+
+func Load() (*Config, error) {
+	cfg := Default()
+	path, err := configPath()
+	if err != nil {
+		return cfg, nil
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return cfg, nil
+		}
+		return cfg, err
+	}
+	json.Unmarshal(data, cfg)
+	return cfg, nil
+}
+
+func Save(cfg *Config) error {
+	path, err := configPath()
+	if err != nil {
+		return err
+	}
+	data, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
