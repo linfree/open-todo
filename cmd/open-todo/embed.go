db14532a58b5312de46e8137efa89b96f8b23abc
linfree <linfree.php@outlook.com>
2026-06-30 12:35:18 +0800

    feat: add Go desktop entry point with embedded frontend support

--- cmd/open-todo/embed.go
+++ cmd/open-todo/embed.go
@@ -1,5 +1,36 @@
 package main
 
-//go:generate go run github.com/kevinburke/go-bindata/go-bindata -o bindata.go -pkg main -prefix "../../web/dist" ../../web/dist/...
+import (
+	"embed"
+	"io/fs"
+	"net/http"
 
-// Embed will be expanded in later tasks to serve the frontend build.
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed web-dist/*
+var webAssets embed.FS
+
+func registerStaticRoutes(r *gin.Engine) {
+	sub, err := fs.Sub(webAssets, "web-dist")
+	if err != nil {
+		// 开发模式: 没有 web-dist 目录, 跳过
+		return
+	}
+
+	fileServer := http.FileServer(http.FS(sub))
+
+	r.GET("/assets/*filepath", func(c *gin.Context) {
+		c.Writer.Header().Set("Cache-Control", "public, max-age=31536000")
+		fileServer.ServeHTTP(c.Writer, c.Request)
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		data, err := webAssets.ReadFile("web-dist/index.html")
+		if err != nil {
+			c.String(http.StatusNotFound, "Frontend not built. Run: cd web && npm run build")
+			return
+		}
+		c.Data(http.StatusOK, "text/html; charset=utf-8", data)
+	})
+}
--- cmd/open-todo/main.go
+++ cmd/open-todo/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/linfree/open-todo/internal/config"
+	"github.com/linfree/open-todo/internal/server"
+	"github.com/linfree/open-todo/internal/store"
+)
 
 func main() {
-	fmt.Println("open-todo starting...")
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	st, err := store.Open(cfg.DatabasePath)
+	if err != nil {
+		log.Fatalf("open store: %v", err)
+	}
+	defer st.Close()
+
+	if err := st.InitDefaults(); err != nil {
+		log.Printf("init defaults: %v", err)
+	}
+
+	router := server.New(st, cfg)
+	registerStaticRoutes(router)
+
+	addr := fmt.Sprintf(":%d", cfg.Port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("listen %s: %v", addr, err)
+	}
+
+	log.Printf("open-todo server starting on http://localhost%s", addr)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigCh
+		log.Println("shutting down...")
+		listener.Close()
+	}()
+
+	if err := router.RunListener(listener); err != nil {
+		log.Printf("server: %v", err)
+	}
 }
